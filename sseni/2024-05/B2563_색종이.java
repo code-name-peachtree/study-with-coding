15901f7fbbb71066bce5445e703851881431ab4a
jsl1113 <jsl1113@naver.com>
2024-05-20 18:21:25 +0900

    2024-05-20

---
+++ sseni/2024-05/B2563_색종이.java
@@ -0,0 +1,37 @@
+package com.example.algorithm.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B2563_색종이 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[100][100];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j] == 1) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
