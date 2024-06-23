8e7c454aa29507b6dc873dca18a4bf80fcaaf572
stacy-sg <tmfrl3589@gmail.com>
2024-06-23 14:41:25 +0900

    2024-06-24

---
+++ seulgi/2024-06/B1149_RGB거리.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+rgb = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0]*3 for _ in range(n)]
+dp[0] = rgb[0]
+
+for i in range(1,n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgb[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgb[i][2]
+
+print(min(dp[n-1]))
