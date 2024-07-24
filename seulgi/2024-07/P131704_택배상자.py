4905926ae9577d0d8f789164390b0da0e0143188
stacy-sg <tmfrl3589@gmail.com>
2024-07-24 19:56:21 +0900

    2024-07-24

---
+++ seulgi/2024-07/P131704_택배상자.py
@@ -0,0 +1,12 @@
+def solution(order):
+    answer = 0
+    belt = []
+    idx = 0
+    for box in range(1, len(order) + 1):
+        belt.append(box)
+        while (belt) and (belt[-1] == order[idx]):
+            answer += 1
+            idx += 1
+            belt.pop()
+
+    return answer
